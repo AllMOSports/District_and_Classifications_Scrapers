543dde10a3bb43b0cc77587f4dc093ad37dba953
AllMOSports <AllMOSports63775@gmail.com>
2026-06-16 15:40:34 -0500

    Update mshsaa_fall_softball_scraper.py

--- mshsaa_fall_softball_scraper.py
+++ mshsaa_fall_softball_scraper.py
@@ -50,7 +50,7 @@ log = logging.getLogger(__name__)
  
 BASE_URL    = "https://www.mshsaa.org/Activities/ClassAndDistrictAssignments.aspx"
 ACTIVITY    = 38
-NUM_CLASSES = 4
+NUM_CLASSES = 5
  
 SEASONS = [
     ("2025-2026", None),
